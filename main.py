6368cc597c51031353ea12dd1f8470b8b95bf461
Sean Berger <seanbe@mta.ac.il>
2025-07-06 19:49:36 +0300

    updated dataset import to use sklearn (cant upload large files to github)

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import pandas as pd
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+import numpy as np
 from neural_network import NeuralNetwork
 
 
 if __name__ == '__main__':
     '''Load MNIST from data folder, train the network, and evaluate accuracy.'''
     # Read and preprocess the MNIST dataset
-    data_train = pd.read_csv("data\MNIST-train.csv").to_numpy()
-    data_test = pd.read_csv("data\MNIST-test.csv").to_numpy()
+    mnist = fetch_openml('mnist_784', version=1, as_frame=False)
+    # stack data + labels into one array, convert targets to ints
+    data = np.column_stack((mnist.data, mnist.target.astype(np.int64)))
+    # split off 10 000 examples for testing
+    data_train, data_test = train_test_split(data, test_size=10000, random_state=42)
     X_train, y_train = data_train[:, :-1], data_train[:, -1]
     X_test, y_test = data_test[:, :-1], data_test[:, -1]
 
